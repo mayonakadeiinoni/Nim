47bc4e02dfe7e4a9986e063443f7ddf6875172aa
sonoda <ytmr0911@gmailcom>
2025-04-06 17:56:28 +0900

    自動フォーマッティング

--- README.md
+++ README.md
@@ -10,3 +10,15 @@
 
 - ドキュメント自動作成ツール　Docstringなど
 https://qiita.com/Jazuma/items/214ebd89cb3954aba454
+https://magazine.techacademy.jp/magazine/30599
+
+- 自動フォーマッティング　autopepe8とか
+ vscodeの機能でできないか？
+　いろいろ試してようやく自動フォーマッティングできた!!
+##勉強したコマンド
+### git
+ #### reset系
+ - ```git reset --soft HEAD~1``` `--soft`はコミットだけを消す，つまり，ステージングは消さない．`HEAD~1` HEADは最新のポインタで，~1はポインタの一つ前のやつのこと．
+ - ```git reset --mixed HEAD~1``` `--mixed`はコミットもステージングも消す．
+ - ```git reset --hard HEAD~1``` `--hard`はすべて消す．
+ 
--- script/Nim.py
+++ script/Nim.py
@@ -1,39 +1,50 @@
+import random
+
+
 class Player:
     """プレイヤーを表す親クラス．
-    
+
     Attributes:
         name (str): プレイヤーを表す名前
-    
-    """
-    def __init__(self,name):
-        self.name=name
 
-    def select_move(self,piles):
-        raise NotImplementedError("This method should be overridden by subclasses")
-    
+    """
+
+    def __init__(self, name):
+        self.name = name
+
+    def select_move(self, piles):
+        raise NotImplementedError(
+            "This method should be overridden by subclasses")
+
+
 class HumanPlayer(Player):
-    
+
     """Playerを継承するクラス．
-    
+
     人間が操作するプレイヤーを表すクラスです．
     Attributes:
         name (str): プレイヤーを表す名前(継承)
     """
-    def select_move(self,mount):
+
+    def select_move(self, mount):
         index = int(input("どの山からとるか"))
-        amount= int(input("山からとる量"))
-        return index,amount
+        amount = int(input("山からとる量"))
+        return index, amount
+
 
 class ComputerPlayer(Player):
-    
+
     """Playerを継承するクラス．
-    
+
     コンピュータが操作するプレイヤーを表すクラスです．
     Attributes:
         name (str): プレイヤーを表す名前(継承)
     """
-    def select_move(self,mount):
-        ## まだ操作は未定．
+
+    def select_move(self, mount):
+        # まだ操作は未定．
+        valid_indexs = [i for i, pile in enumerate(mount) if pile > 0]
+
         index = int(0)
-        amount= int(0)
-        return index,amount
+        amount = int(0)
+        return index, amount
